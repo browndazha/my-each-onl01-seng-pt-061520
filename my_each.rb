270a3d094f20190cc85d72994faa39c72ddd6dfe
Dazha Brown <dajahbrown@hotmail.com>
2020-06-28 21:45:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each
+  ruby collection = [1, 2, 3, 4]
+  my_each(collection) do |i|
+    puts i
+    while true
+        yield
+    end
 end
